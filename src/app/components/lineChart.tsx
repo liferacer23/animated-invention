1d461ac8c11e200ca5e60bd1d6821245fb49a99f
binyam <binyamg23@gmail.com>
2024-01-20 00:52:18 +0300

    deployment fix

--- src/app/components/lineChart.tsx
+++ src/app/components/lineChart.tsx
@@ -1,4 +1,5 @@
 "use client";
+import moment from "moment";
 import React from "react";
 import {
   LineChart,
@@ -10,51 +11,6 @@ import {
   Legend,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 interface ChartData {
   createdAt: string;
   activeBrokers: number;
@@ -64,20 +20,37 @@ export default function App({ chartData }: { chartData: ChartData[] }) {
   console.log(chartData);
   return (
     <LineChart
-      width={500}
+      width={600}
       height={300}
       data={chartData}
       margin={{
         top: 5,
         right: 30,
-        left: 20,
+        left: 30,
         bottom: 5,
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="createdAt" />
-      <YAxis />
-      <Tooltip />
+      <XAxis
+        tickFormatter={(tick) => {
+          return moment(tick).format("MMM");
+        }}
+        dataKey="createdAt"
+        label={{ value: "Month", position: "insideBottomRight", offset: 0 }}
+      />
+      <YAxis
+        label={{
+          value: "Total Brokers",
+          angle: -90,
+          position: "insideLeft",
+          offset: -20,
+        }}
+      />
+      <Tooltip
+        labelFormatter={(label) => {
+          return moment(label).format("MMM");
+        }}
+      />
       <Legend />
       <Line
         type="monotone"
